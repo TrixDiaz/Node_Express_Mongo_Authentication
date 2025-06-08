591507c50e22850c09daa56f78c53213c4139e74
XirTech <john.darlucio022@gmail.com>
2025-06-08 18:20:34 +0800

    authentication

---
+++ routes/user.routes.js
@@ -0,0 +1,32 @@
+import {Router} from "express";
+import {
+    deleteUser,
+    getUser,
+    getUsers,
+    storeUser,
+    updateUser,
+    updateUserPassword
+} from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
+
+const userRouter = Router();
+
+// Fetch all users
+userRouter.get("/", getUsers);
+
+// Fetch a single user by ID
+userRouter.get("/:id", authorize, getUser);
+
+// Create a new user
+userRouter.post("/", authorize, storeUser);
+
+// Update an existing user by ID
+userRouter.put("/:id", authorize, updateUser);
+
+// Update an existing user  password by ID
+userRouter.put("/:id/password", authorize, updateUserPassword);
+
+// Delete a user by ID
+userRouter.delete("/:id", authorize, deleteUser);
+
+export default userRouter;
---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import connectToDatabase from "./database/mongodb.js";
+import {PORT} from "./config/env.js";
+
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(arcjetMiddleware);
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+
+app.get('/', (res) => {
+   res.send('Hello, World! Welcome to the Server from Node and Express!');
+});
+
+app.listen(PORT, async () => {
+   console.log(`Server started on port ${PORT}`);
+   await connectToDatabase();
+});
